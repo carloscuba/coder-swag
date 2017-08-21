92c7e7aa116dbdb45165da812179d3ca64e76505
Carlos Cuba <carlos.cuba@bonzzu.com>
2017-08-21 01:42:35 -0500

    Main storyboard updated: Products view created and outlets added. ProductCell created.

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Carlos Cuba on 8/21/17.
+//  Copyright © 2017 Carlos Cuba. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var procuctPrice: UILabel!
+    
+}
